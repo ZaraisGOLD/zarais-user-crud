dd6edcf231ab8defa345520834afee7547679b48
ZaraisGOLD <zaraisgold@gmail.com>
2023-06-05 03:19:00 -0400

    finish

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -4,8 +4,8 @@ const express = require('express');
 const userRouter = express.Router();
 
 userRouter.route("/")
-		.get(getAll)
-        .post(create)
+    .get(getAll)
+    .post(create)
 
 userRouter.route('/:id')
     .get(getOne)
